ce82d6dbeb332111c8fead6d44e628758e61db09
Benjamin Ly <benjaminly702@gmail.com>
2024-08-23 05:36:46 -0700

    Added base files and configured speech to read txt file

---
+++ quickstart.py
@@ -0,0 +1,43 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+
+# <code>
+import azure.cognitiveservices.speech as speechsdk
+import dotenv, os
+
+dotenv.load_dotenv()
+# Creates an instance of a speech config with specified subscription key and service region.
+# Replace with your own subscription key and service region (e.g., "westus").
+speech_key = os.getenv('AZURE_RSC_SPEECH_KEY')
+service_region = os.getenv('AZURE_RSC_SPEECH_REGION')
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+# Set the voice name, refer to https://aka.ms/speech/voices/neural for full list.
+speech_config.speech_synthesis_voice_name = "en-US-LunaNeural"
+
+# Creates a speech synthesizer using the default speaker as audio output.
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+# Receives a text from console input.
+# print("Type some text that you want to speak...")
+# text = input()
+
+# Reads a text file
+with open('./fabric.txt', 'r') as f:
+    text = f.read()
+
+# Synthesizes the received text to speech.
+# The synthesized speech is expected to be heard on the speaker with this line executed.
+result = speech_synthesizer.speak_text_async(text).get()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized to speaker for text [{}]".format(text))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+    print("Did you update the subscription info?")
+# </code>
